a2016dda4309482f9d85b49a8afbfdb1d7e41fe1
lybned <60757167+lybned@users.noreply.github.com>
2021-11-21 19:01:54 -0700

    Added:basic login page (but you can't login yet)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <header>
+
+        <h1>CPSC 471 Website</h1>
+
+      </header>   
+      <form class = "form">    
+        <section id="loginUser">
+          
+          <label for="username">Username</label>
+          <input id="username" name="username" type="text" />
+
+        </section>
+
+        <section id="loginPass">
+          <label for="password">Password</label>
+          <input id="password" name="password" type="text" />
+        </section>
+
+        <section id="login">
+          <input type="submit" value="Login" />
+        </section>
+      </form>
     </div>
+
   );
 }
 
 export default App;
+
