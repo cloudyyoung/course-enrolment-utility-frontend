09736802893f7b19bf3547b09de1a19acdc4bf2f
lybned <60757167+lybned@users.noreply.github.com>
2021-11-22 00:14:04 -0700

    Added space: Login.js
    Added space to make Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 
-
+//the style for the title of the website
 const ssss = {
     textAlign: 'center',
     color: 'blue'
 };
 
+//function that returns the HTML code 
 const Login = () => {
 
   
@@ -21,18 +22,18 @@ const Login = () => {
         <form class = "form" style={{textAlign: 'center'}}>    
             <section id="loginUser">
             
-            <label for="username">Username</label>
-            <input id="username" name="username" type="text" maxlength="20" minlength="5"/>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" maxlength="20" minlength="5"/>
     
             </section>
     
             <section id="loginPass">
-            <label for="password">Password</label>
-            <input id="password" name="password" type="password" maxlength="20" minlength="5"/>
+                <label for="password">Password</label>
+                <input id="password" name="password" type="password" maxlength="20" minlength="5"/>
             </section>
     
             <section id="login">
-            <input type="submit" value="Login" />
+                <input type="submit" value="Login" />
             </section>
         </form>
     
