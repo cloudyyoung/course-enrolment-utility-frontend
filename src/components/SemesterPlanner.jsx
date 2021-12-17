9af50fa93ccbc970c1585587b192ce39c52052a6
Cloudy Young <cloudy.young@outlook.com>
2021-12-17 14:16:05 -0700

    Update right panel

--- src/components/SemesterPlanner.jsx
+++ src/components/SemesterPlanner.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { Container, Draggable } from "react-smooth-dnd";
 
+
 class SemesterPlanner extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             semester: "",
             courses: [
+                {
+                    course_id: 4954,
+                    no_gpa: false,
+                    repeat: false,
+                    code: "CPSC",
+                    number: "359",
+                    unit: 3,
+                    topic: "Computing Machinery II",
+                    description: "An introduction to hardware and microprocessor design, including the connection between gate-level digital logic circuits and sequential machines that can execute an algorithm and perform input and output. This course may not be repeated for credit.",
+                    hours: "H(1-3H)",
+                    prerequisites: "CPSC 355 and PHIL 279 or 377",
+                    antirequisites: "Credit for both Computer Science 359 and any of 325, 455 or Computer Engineering 415 will not be allowed.",
+                    corequisites: "",
+                    notes: "",
+                    aka: "",
+                    time_length: "",
+                },
                 {
                     course_id: 4954,
                     no_gpa: false,
@@ -33,7 +51,7 @@ class SemesterPlanner extends React.Component {
         for (let course of this.state.courses) {
             dom.push(
                 <Draggable key={course.course_id}>
-                    <div className="card">
+                    <div className="card course">
                         <div className="card-content">
                             <h2 className="title is-6">{course.code.toUpperCase()} {course.number}</h2>
                             <p className="subtitle is-7">{course.topic}</p>
@@ -54,7 +72,7 @@ class SemesterPlanner extends React.Component {
                     </select>
                 </div>
                 <div>
-                    <Container removeOnDropOut={true} dropPlaceholder={true}>
+                    <Container removeOnDropOut={true} dropPlaceholder={{className: "drag-placeholder"}}>
                         {dom}
                     </Container>
                 </div>
