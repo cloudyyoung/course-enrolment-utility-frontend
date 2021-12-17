d979fa778230f28c7980565ffbb3a7b040e24529
Cloudy Young <cloudy.young@outlook.com>
2021-12-17 03:31:39 -0700

    Update tree page

--- src/pages/Tree.jsx
+++ src/pages/Tree.jsx
@@ -6,32 +6,30 @@ const elements = [
         id: "1",
         type: "input", // input node
         data: { label: "Input Node" },
-        position: { x: 250, y: 25 },
+        position: { x: 250, y: 125 },
     },
     // default node
     {
         id: "2",
         // you can also pass a React component as a label
         data: { label: <div>Default Node</div> },
-        position: { x: 100, y: 125 },
+        position: { x: 100, y: 325 },
     },
     {
         id: "3",
         type: "output", // output node
         data: { label: "Output Node" },
-        position: { x: 250, y: 250 },
+        position: { x: 250, y: 450 },
     },
     // animated edge
-    { id: "e1-2", source: "1", target: "2", animated: true },
-    { id: "e2-3", source: "2", target: "3" },
+    { id: "e1-2", source: "1", target: "2", type: "step" },
+    { id: "e2-3", source: "2", target: "3", type: "step", animated: true},
 ];
 
 
 const Tree = () => {
     return (
-        <div style={{ height: 300 }}>
-            <ReactFlow elements={elements} />
-        </div>
+        <ReactFlow elements={elements} />
     );
 };
 
