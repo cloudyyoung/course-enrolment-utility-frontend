4dab5c7f2780d5c3f74b8e698591f232e1854cd8
Cloudy Young <cloudy.young@outlook.com>
2021-12-17 11:10:21 -0700

    Update Statistics.jsx

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-export default function Statistics() {
-    return (
-        <div className="container">
-            <h1 className="is-text-4"></h1>
-            <div className="card">
-                <div className="card-content">
-                    <h2 className="title is-2 grade">0</h2>
-                    <p className="subtitle">Total Users</p>
-                    <p className="description">The total number of users in the database, including both students and administrators.</p>
+class Statistics extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            totalUsers: 0,
+            totalCourses: 0,
+        };
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="is-text-4"></h1>
+                <div className="card">
+                    <div className="card-content">
+                        <h2 className="title is-2 grade">{ this.state.totalUsers }</h2>
+                        <p className="subtitle">Total Users</p>
+                        <p className="description">The total number of users in the database, including both students and administrators.</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card-content">
+                        <h2 className="title is-2 grade">{ this.state.totalCourses }</h2>
+                        <p className="subtitle">Total Courses</p>
+                        <p className="description">The total number of courses in the database.</p>
+                    </div>
                 </div>
             </div>
-            <div className="card">
-                <div className="card-content">
-                    <h2 className="title is-2 grade">0</h2>
-                    <p className="subtitle">Total Courses</p>
-                    <p className="description">The total number of courses in the database.</p>
-                </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
+
+export default Statistics;
