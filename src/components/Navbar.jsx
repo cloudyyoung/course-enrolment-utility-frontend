42a6702475c3377fd17f8027762f524a4cf6bc0e
Cloudy Young <cloudy.young@outlook.com>
2021-12-18 13:50:11 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,65 @@
 import React from "react";
 
-export default function Navbar() {
-    return (
-        <nav className="navbar is-transparent has-shadow">
-            <div className="container">
-                <div className="navbar-brand">
-                    <a className="navbar-item" href="/">Ucalgary</a>
-                    <div className="navbar-burger" data-target="navbarExampleTransparentExample">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </div>
+class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+        };
+    }
 
-                <div id="navbarExampleTransparentExample" className="navbar-menu">
-                    <div className="navbar-start">
-                        <a className="navbar-item" href="/tree">Course Tree</a>
-                        <a className="navbar-item" href="/list">Course List</a>
+    componentDidMount() {
+        this.setState({ email: localStorage.getItem("email") });
+    }
+
+    render() {
+        return (
+            <nav className="navbar is-transparent has-shadow">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a className="navbar-item" href="/">Ucalgary</a>
+                        <div className="navbar-burger" data-target="navbarExampleTransparentExample">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
                     </div>
 
-                    <div className="navbar-end">
-                        <div className="navbar-item">
-                            <div className="buttons">
-                                <a className="button is-primary" href="/signup">
-                                    <strong>Sign up</strong>
-                                </a>
-                                <a className="button is-light" href="/signin">
-                                    Sign in
-                                </a>
+                    <div id="navbarExampleTransparentExample" className="navbar-menu">
+                        <div className="navbar-start">
+                            <a className="navbar-item" href="/tree">Course Tree</a>
+                            <a className="navbar-item" href="/list">Course List</a>
+                        </div>
+
+                        <div className="navbar-end">
+                            <div className="navbar-item">
+                                {
+                                    !this.state.email && 
+                                    <div className="buttons">
+                                        <a className="button is-primary" href="/signup">
+                                            <strong>Sign up</strong>
+                                        </a>
+                                        <a className="button is-light" href="/signin">
+                                            Sign in
+                                        </a>
+                                    </div>
+                                }
+                                {
+                                    this.state.email &&
+                                    <div className="buttons">
+                                        <a className="button is-light" href="/">
+                                            {this.state.email}
+                                        </a>
+                                    </div>
+                                }
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    );
+            </nav>
+        );
+    }
 }
+
+
+export default Navbar;
