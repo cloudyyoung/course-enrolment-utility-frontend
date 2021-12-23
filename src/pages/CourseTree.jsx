03c2a1a1eae00b9c8aa2c04c98cc7fb42c544a7a
Cloudy Young <cloudy.young@outlook.com>
2021-12-23 04:27:41 -0700

    Update CourseTree.jsx

--- src/pages/CourseTree.jsx
+++ src/pages/CourseTree.jsx
@@ -2,17 +2,21 @@ import React from "react";
 import Tree from "../components/Tree";
 import EnrolmentPlanner from "../components/EnrolmentPlanner";
 
+const Context = React.createContext();
+
 class CourseTree extends React.Component {
     render() {
         return (
-            <div className="columns tree-page">
-                <div className="column is-9">
-                    <Tree />
+            <Context.Provider value={{ state: this.state, setCourses: (courses) => { this.setState({ courses: courses }); } }}>
+                <div className="columns tree-page">
+                    <div className="column is-9">
+                        <Tree />
+                    </div>
+                    <div className="column is-3 right-panel">
+                        <EnrolmentPlanner />
+                    </div>
                 </div>
-                <div className="column is-3 right-panel">
-                    <EnrolmentPlanner />
-                </div>
-            </div>
+            </Context.Provider>
         );
     }
 }
